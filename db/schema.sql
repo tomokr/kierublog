049798c7f878439fcd7538527833ebf7b47980db
tomokr <tomok@Tomo-no-MacBook-Air.local>
2014-08-13 15:20:14 +0900

    スキーマできました

---
+++ db/schema.sql
@@ -0,0 +1,19 @@
+CREATE TABLE user (
+    `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    `name` VARBINARY(32) NOT NULL,
+
+    PRIMARY KEY(id),
+    UNIQUE KEY (name)
+);
+
+CREATE TABLE entry (
+    `id` BIGINT UNSIGNED NOT NULL AUTO_INCREMENT,
+    `user_id` BIGINT UNSIGNED NOT NULL,
+    `title` VARBINARY(256) NOT NULL,
+    `text` VARBINARY(512) NOT NULL,
+    `created` TIMESTAMP NOT NULL DEFAULT '0000-00-00 00:00:00',
+    `date` DATE NOT NULL DEFAULT '0000-00-00',
+
+    PRIMARY KEY(id),
+    KEY (user_id, date, created)
+);
