ca9a85eaee1218acc5ea8fb15c9aad79d213f399
tomokr <tomok@Tomo-no-MacBook-Air.local>
2014-08-15 22:53:30 +0900

    ニュートンがふりました

--- mouseStalker/main.js
+++ mouseStalker/main.js
@@ -1,4 +1,8 @@
 var mousex, mousey;
+var counter_id = define_counter();
+var animation_id = define_counter();
+var g = 1; //重力
+var window_height = $( window ).height();
 
 document.onmousemove=function(e){
 		document.getElementById("Mo").style.left=e.pageX+15+"px";
@@ -9,15 +13,42 @@ document.onmousemove=function(e){
 
 document.addEventListener('click',function(e){
 	document.body.appendChild(createImg());
+	animateImg();
 },false);
 
-function createImg(left){
+function createImg(){
         var imgDivElem = document.createElement("div");
         var imgElem = imgDivElem.appendChild(document.createElement("img"));
+        imgDivElem.id = "img"+counter_id();
         imgElem.src="newton.jpg";
         imgDivElem.style.position = "absolute";
         imgDivElem.style.left = mousex+15+"px";
-        imgDivElem.style.bottom = "0px";
+        imgDivElem.style.top = mousey+15+"px";
 
         return imgDivElem;
 }
+
+function animateImg(){
+	var imgDivElem = document.getElementById("img"+animation_id());
+	var time = 0;
+	var top = parseInt((imgDivElem.style.top).replace("px",""));
+	var intervalID = setInterval(function (){
+		if(top > window_height-146){
+			clearInterval(intervalID);
+			return;
+		}
+		console.log("a");
+		time++;
+		top += time*g;
+		imgDivElem.style.top = top+"px";
+	}, 50);
+
+}
+
+function define_counter() {
+    var i = 0;
+    return function() {
+      return ++i;
+    };
+};
+
