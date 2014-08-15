c4e66a9b0f66e68bb9bc9c1b0ad8b93c11e1ef07
tomokr <tomok@Tomo-no-MacBook-Air.local>
2014-08-15 15:39:27 +0900

    APIでとってくる件数などを指定できるようになった

--- lib/Intern/Diary/Engine/API.pm
+++ lib/Intern/Diary/Engine/API.pm
@@ -14,18 +14,22 @@ sub entries{
     my ($class, $c) = @_;
     warn "entries";
 
+    my $per_page = $c->req->parameters->{per_page};
+    my $page = $c->req->parameters->{page};
+    my $offset = ($page - 1) * $per_page;
+
     #user=tomokの場合
     my $user = Intern::Diary::Service::DB::User->find_user_by_name($c->db, {name=>'tomok',});
 
-    my $entries = Intern::Diary::Service::DB::Entry->find_entries_by_user_id($c->db,$user->id);
+    my $entries = Intern::Diary::Service::DB::Entry->find_entries_by_user_id($c->db,$user->id,{limit=>$per_page, offset=>$offset});
     my @hash_entries = map {
     	Intern::Diary::Model::Entry->TO_HASH($_)
     	} @$entries;
 
     $c->json({
     	entries => \@hash_entries,
-    	# per_page => JSON::Types::number $per_page,
-    	# next_page => JSON::Types::number $page + 1,
+    	per_page => JSON::Types::number $per_page,
+    	next_page => JSON::Types::number $page + 1,
     	});
 
 }
