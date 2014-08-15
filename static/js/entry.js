962ac49d05faa74d94913423318f8e29e38c7c88
tomokr <tomok@Tomo-no-MacBook-Air.local>
2014-08-15 17:41:08 +0900

    次にだけすすめるページャ実装した

--- static/js/entry.js
+++ static/js/entry.js
@@ -1,7 +1,12 @@
 /**
  * XHR でサーバーからブックマークの情報を取得してブックマーク一覧を更新。
  */
-function updateContent() {
+var nowpage, per_page;
+
+function updateContent(page, per_page) {
+
+    nowpage = page;
+    per_page = per_page;
 
     var contentElem = document.getElementById("content");
     contentElem.innerHTML = "";
@@ -16,6 +21,12 @@ function updateContent() {
         var textElem = itemElem.appendChild(document.createElement("div"));
         textElem.innerHTML = (e.text).replace(/\r\n/g,"<br>");
 
+        var brElem = itemElem.appendChild(document.createElement("br"));
+
+         var linkElem = itemElem.appendChild(document.createElement("a"));
+         linkElem.href = "./entry?id="+e.id;
+        linkElem.innerHTML = "この記事を編集/削除する";
+
         return itemElem;
     }
 
@@ -27,8 +38,19 @@ function updateContent() {
         contentElem.innerHTML = "<div class=\"message\">ブックマーク一覧を読み込めませんでした。</div>"
     }
 
+ //   記事ページを動かすためのボタン
+　　　function createLinks(){
+        var linksDivElem = document.createElement("div");
+        var linksElem = linksDivElem.appendChild(document.createElement("a"));
+        linksElem.href = "javascript:void(0);";
+        linksElem.onclick = function () {updateContent((nowpage+1),per_page)};
+        linksElem.innerHTML = "次へ";
+        var linksElem
+        return linksDivElem;
+ 　　　}
+
     var xhr = new XMLHttpRequest();
-    xhr.open("GET", "/api/entries", true);
+    xhr.open("GET", "/api/entries?page="+page+"&per_page="+per_page, true);
     xhr.addEventListener("load", function (evt) {
         contentElem.innerHTML = "";
         var xhr = evt.currentTarget;
@@ -36,7 +58,9 @@ function updateContent() {
             var entriesInfo = JSON.parse(xhr.responseText);
             entriesInfo.entries.forEach(function (e) {
                 contentElem.appendChild(createEntryItemElem(e));
-            });
+            }
+            );
+            contentElem.appendChild(createLinks());
         } else {
             showError();
         }
@@ -44,9 +68,13 @@ function updateContent() {
     xhr.send();
     showProgress();
 
-}
+}//updateContentおわり
+
+    function test () {
+        alert("hello");
+    }
 
 // ページ読み込み時の初期化処理
 document.addEventListener("DOMContentLoaded", function (evt) {
-    updateContent();
+    updateContent(1,3);
 }, false);
